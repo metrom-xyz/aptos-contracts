6ab5a95699865bc0f1121ae13ceec456d02ed716
luzzif <federico.luzzi@proton.me>
2025-09-07 12:02:16 +0200

    feat: new deployment in mainnet

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export interface ChainContract {
     address: `0x${string}`;
-    blockCreated: number;
+    versionCreated: number;
 }
 
 export enum SupportedChain {
@@ -12,11 +12,11 @@ export const ADDRESS: Record<SupportedChain, ChainContract> = {
     [SupportedChain.Testnet]: {
         address:
             "0xb0359fb67f74ce2dd11c9b219be4b2f00569e6b55912e2145a8e69df1497ce67",
-        blockCreated: 6854139736,
+        versionCreated: 6854139736,
     },
     [SupportedChain.Mainnet]: {
         address:
-            "0xd438cb1c136b04beb5b832d2ab1e63c73d1d1a95fe3e8eb1a79d1e86b746a42b",
-        blockCreated: 3353134305,
+            "0x0244c176d3d3db102bc46581cd22050e371afdce03c58307507ce6eef450d166",
+        versionCreated: 3362403011,
     },
 };
